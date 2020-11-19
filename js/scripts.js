dd4906780aec473e6fae0f76761e0c0a8cb83cb2
w3bwizard <w3bwiz4rd@gmail.com>
2020-11-19 23:35:23 +0200

    полностью заадаптивил главную страницу, минимально поправил контент и разметку

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,6 @@
-$(document).ready(function() {
-//Показать меню при клике на гамбургер   
-    $('.hamburger, .hidden_top_menu').click(function() {
-        if ($('.hidden_top_menu').is(':hidden')){
-            $('.hidden_top_menu').css('display','flex');
-        } else {
-            $('.hidden_top_menu').hide();
-        }
-    });
-});
-$(document).ready(function() {
-//Скрыть блок по таймеру
-    $('.load-block1').delay(3000).fadeOut();
-//Показать блок по таймеру
-   setTimeout(function(){ $('.load-block2').css('display','flex')},4000);
-});
-
+window.onload = function () {
+    let x = document.querySelector(".menu");
+    document.querySelector("#menu__toogle").onclick = function () {
+        x.classList.toggle('menu__popup');
+    }
+}
